e22c678143be553439062fcffdf00392d05187d7
JornLee <sbpfikdm@gmail.com>
2026-04-22 12:47:04 +0800

    ??

--- tools/设置代理.bat
+++ tools/设置代理.bat
@@ -1,7 +1,7 @@
 @ECHO OFF & color 0a
 
-git config http.proxy  http://127.0.0.1:6600
-git config https.proxy http://127.0.0.1:6600
+git config http.proxy  http://10.6.6.102:6600
+git config https.proxy http://10.6.6.102:6600
 
 echo 【%date% %time%】 代理设置成功 ..
 
