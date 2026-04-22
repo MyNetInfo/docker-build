c0eda4560ace82ea6cc4d55fd1e16d04fc9459d5
JornLee <sbpfikdm@gmail.com>
2026-04-22 19:33:04 +0800

    ??

---
+++ scripts/alpine/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source /etc/profile && HostName="`cat /etc/hostname`"
+echo "--------------------------------------------------- `date +%Y-%m-%d` `date +%H:%M:%S` --------------------------------------------------- ${HostName}.Start"
+
+# 权限
+sudo chmod -R 0700      /root/*.sh
+sudo chown -R root:root /root/*.sh
+
+# 权限
+sudo chmod -R 0700      /root/shell
+sudo chown -R root:root /root/shell
+
+# 权限
+sudo chmod -R 0700      /root/scripts
+sudo chown -R root:root /root/scripts
+
+# 启动服务
+sudo /usr/sbin/sshd -D &
+
+while true; do
+    echo "`date +%Y-%m-%d` `date +%H:%M:%S` ${HostName} .." > /tmp/start.log
+    netstat -tunlp | sort -n -k4.2 | grep "LISTEN" >> /tmp/start.log
+    sleep 3
+done
+
+echo "--------------------------------------------------- `date +%Y-%m-%d` `date +%H:%M:%S` --------------------------------------------------- ${HostName}.Start.Exit" && exit
+
+clear && chmod -R 0700 && /root/start.sh
--- scripts/debian/shell/start.sh
+++ scripts/debian/shell/start.sh
@@ -2,14 +2,24 @@
 source /etc/profile && HostName="`cat /etc/hostname`"
 echo "################################################### `date +%Y-%m-%d` `date +%H:%M:%S` ################################################### start"
 
+# 权限
+chmod -R 0700      /root/*.sh
+chown -R root:root /root/*.sh
+
+# 权限
+chmod -R 0700      /root/shell
+chown -R root:root /root/shell
+
+# 权限
+chmod -R 0700      /root/scripts
+chown -R root:root /root/scripts
+
+# 启动服务
 /etc/init.d/ssh start
 /etc/init.d/ssh restart
 
 # svnserve -d -r /data/SvnData
 # /usr/bin/svnserve -d -r /data/SvnData
-
-curl -4 ifconfig.me
-curl -6 ifconfig.me
 
 while true; do
     echo "`date +%Y-%m-%d` `date +%H:%M:%S` ${HostName} .." > /tmp/start.log
