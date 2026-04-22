52a52530bc04830540203c748173832240bf585e
JornLee <sbpfikdm@gmail.com>
2026-04-22 20:54:25 +0800

    ??

--- docs/示例-alpine.sh
+++ docs/示例-alpine.sh
@@ -6,5 +6,5 @@ set RunParams= -itd --restart=always --cpus=0.2 --memory=256m --network LdMain -
 docker rm -f Alpine322
 docker rm -f Alpine323
 
-docker run %RunParams% --name Alpine322 --hostname Alpine322 -v E:/DockerData/Alpine322.tmp:/tmp -p 2260:22/tcp mynetinfo/ubuntu1804:1.0
-docker run %RunParams% --name Alpine323 --hostname Alpine323 -v E:/DockerData/Alpine323.tmp:/tmp -p 2261:22/tcp mynetinfo/ubuntu2004:1.0
+docker run %RunParams% --name Alpine322 --hostname Alpine322 -v E:/DockerData/Alpine322.tmp:/tmp -p 2260:22/tcp mynetinfo/alpine322:1.0
+docker run %RunParams% --name Alpine323 --hostname Alpine323 -v E:/DockerData/Alpine323.tmp:/tmp -p 2261:22/tcp mynetinfo/alpine322:1.0
