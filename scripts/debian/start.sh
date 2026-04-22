c74fd2d1cd500f985a6a7caecd859b7bc9dd2531
JornLee <sbpfikdm@gmail.com>
2026-04-22 17:17:32 +0800

    ??

--- scripts/debian/start.sh
+++ scripts/debian/start.sh
@@ -3,9 +3,15 @@ source /etc/profile && HostName="`cat /etc/hostname`"
 echo "################################################### `date +%Y-%m-%d` `date +%H:%M:%S` ################################################### start"
 
 /etc/init.d/ssh start
-/etc/init.d/ssh restart
+
+# svnserve -d -r /data/SvnData
+
+# 备份 (保险)
+# /etc/init.d/ssh restart
+# /usr/bin/svnserve -d -r /data/SvnData
 
 while true; do
     echo "`date +%Y-%m-%d` `date +%H:%M:%S` ${HostName} .." > /tmp/start.log
+    netstat -tunlp | sort -n -k4.2 | grep "LISTEN" > /tmp/start.log
     sleep 3
 done
