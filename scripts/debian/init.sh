a6708e5b218dc8dfbec6011b7c04b83bb0349c70
JornLee <sbpfikdm@gmail.com>
2026-05-19 02:02:12 +0800

    ??

--- scripts/debian/init.sh
+++ scripts/debian/init.sh
@@ -2,20 +2,12 @@
 source /etc/profile && HostName="`cat /etc/hostname`"
 echo "################################################### `date +%Y-%m-%d` `date +%H:%M:%S` ################################################### init"
 
-mkdir -p /root/.ssh
+# 下载
 mkdir -p /root/shell
-
-# 下载 - 该自增编号来自动发布: 002
-wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/authorized_keys
-wget -O /root/shell/install.sh     https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/install.sh
-
-# 权限
-chmod -R 0700 /root/*.sh
-chmod -R 0700 /root/shell/*.sh
-chmod -R 0400 /root/.ssh/authorized_keys
+wget -O  /root/shell/install.sh https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/install.sh
 
 # 初始化
-/root/shell/install.sh
+chmod -R 0700 /root/shell && /root/shell/install.sh
 
 # 清理
 apt-get -y clean
--- scripts/debian/shell/install.sh
+++ scripts/debian/shell/install.sh
@@ -2,17 +2,22 @@
 source /etc/profile && HostName="`cat /etc/hostname`"
 echo "################################################### `date +%Y-%m-%d` `date +%H:%M:%S` ################################################### install"
 
-wget -O /root/ip.sh          https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/ip.sh
-wget -O /root/shell/alias.sh https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/alias.sh
-wget -O /root/shell/soft.sh  https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/soft.sh
-wget -O /root/shell/start.sh https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/start.sh
+mkdir -p /root/.ssh
+mkdir -p /root/shell
 
-# 权限
+wget -O /root/shell/alias.sh https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/alias.sh
+wget -O /root/ip.sh          https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/ip.sh
+wget -O /root/shell/soft.sh  https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/soft.sh
+wget -O /root/shell/ssh.sh   https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/ssh.sh
+wget -O /root/shell/start.sh https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/debian/shell/start.sh
 chmod -R 0700 /root/*.sh
 chmod -R 0700 /root/shell/*.sh
+
+# 密钥
+wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/MyNetInfo/docker-build/refs/heads/main/scripts/authorized_keys
 chmod -R 0400 /root/.ssh/authorized_keys
 
 # 初始化
-/root/shell/alias.sh
-/root/shell/ssh.sh
 /root/shell/soft.sh
+/root/shell/ssh.sh
+/root/shell/alias.sh
